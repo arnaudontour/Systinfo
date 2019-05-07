62c9397e8361793a414420acb552739b32d38eb8
arnaudontour <48843478+arnaudontour@users.noreply.github.com>
2019-05-07 23:30:02 +0200

    Add files via upload

--- cracker/src/cracker.c
+++ cracker/src/cracker.c
@@ -24,11 +24,17 @@
 //INITIALISATION DES MUTEX ET SEMAPHORE
 
 pthread_mutex_t mutex;
+pthread_mutex_t mutex2;
 sem_t empty;
 sem_t full;
+sem_t empty2;
+sem_t full2;
+pthread_mutex_init(&mutex2,NULL);
 pthread_mutex_init(&mutex, NULL);
-sem_init (&empty,0,N-1);
+sem_init (&empty,0,N);
 sem_init(&full,0,0);
+sem_init (&empty2,0,N);
+sem_init(&full2,0,0);
 
 //ARRAYLIST BUFFER 1 (32bytes)
 
@@ -96,69 +102,75 @@ void suppression(Liste *liste)
 
 //ARRAYLIST BUFFER 2 (liste avec les traductions)
 
-typedef struct Element1 Element1;
-struct Element1
+typedef struct Element2 Element2;
+struct Element2
 {
-    Element1 *suivant;
+    Element2 *suivant;
     char trad[16];
 };
 
 
-typedef struct Liste1 Liste1;
-struct Liste1
+typedef struct Liste2 Liste2;
+struct Liste2
 {
-    Element1 *premier;
+    Element2 *premier;
 };
 
-Liste *initialisation()
+Liste2 *initialisation2()
 {
-    Liste *liste = malloc(sizeof(*liste));
-    Element *element = malloc(sizeof(*element));
+    Liste2 *liste2 = malloc(sizeof(*liste2));
+    Element2 *element2 = malloc(sizeof(*element2));
 
     if (liste == NULL || element == NULL)
     {
         exit(EXIT_FAILURE);
     }
 
-    element->hash = NULL;
+    element2->trad = NULL;
     element->suivant = NULL;
-    liste->premier = element;
+    liste2->premier = element;
 
-    return liste;
+    return liste2;
 }
 
 
-void insertion(Liste *liste, uint8_t *value)
+void insertion2(Liste2 *liste2, char[16] trad)
 {
     /* Création du nouvel élément */
-    Element *nouveau = malloc(sizeof(*nouveau));
-    if (liste == NULL || nouveau == NULL)
+    Element2 *nouveau = malloc(sizeof(*nouveau));
+    if (liste2 == NULL || nouveau == NULL)
     {
         exit(EXIT_FAILURE);
     }
-    nouveau->hash = *value;
+    nouveau->trad = trad;
 
     /* Insertion de l'élément au début de la liste */
-    nouveau->suivant = liste->premier;
-    liste->premier = nouveau;
+    nouveau->suivant = liste2->premier;
+    liste2->premier = nouveau;
 }
 
-void suppression(Liste *liste)
+void suppression2(Liste2 *liste2)
 {
-    if (liste == NULL)
+    if (liste2 == NULL)
     {
         exit(EXIT_FAILURE);
     }
 
-    if (liste->premier != NULL)
+    if (liste2->premier != NULL)
     {
-        Element *aSupprimer = liste->premier;
-        liste->premier = liste->premier->suivant;
+        Element2 *aSupprimer = liste2->premier;
+        liste2->premier = liste2->premier->suivant;
         free(aSupprimer);
     }
 }
 
-//ARRAYMIST BUFFER 3 (List avec X nombre de voyelle ou consonne)
+//DELETE POUR LE BUFFER 3 (List avec X nombre de voyelle ou consonne)
+void delete(Liste2 *liste2){
+    while(liste2->premier->suivant != NULL){
+        suppression2(list2);
+    }
+}
+
 
  //Premier producteur
 
@@ -192,24 +204,59 @@ void* producer(void*arg){
 
 // premier consommateur
 
-void consumer(void){
+void* consumer(void*arg){
 	while(true){
 	sem_wait(&full);
 	pthread_mutex_lock(&mutex);
-
-
-
+    char *x[16]; 
+    reversehash(list1->premier->hash, x, sizeof(char*16));
+    sem_wait(&empty2)
+    pthread_mutex_lock(&mutex2);
+    insertion2(liste2, x);
+    suppression2(Liste2 *liste2);
+    pthread_mutex_unlock(&mutex2);
+    sem_post(&full);
 	pthread_mutex_unlock(&mutex);
 	sem_post(&empty);
 	}
 }
- 
+
+void* consumer2(void*){  // ICI ON VA METTRE QU'UN THREAD DU COUP BALLEC
+sem_wait(&full);
+pthread_mutex_lock(&mutex2);
+char trans = list2->premier->trad;
+pthread_mutex_unlock(&mutex2);
+sem_post(&empty2);
+
+int acc = 0;
+if(consonne){
+acc = consonne(trans);
+    if(acc == nmb){
+        insertion3(list3, trans);
+    }
+    if(acc > nmb){
+        delete(list3);
+        insertion3(list3,trans);
+    }
+    else{}
+}
+else{ 
+   acc = voyelle(trans);
+    if(acc == nmb){
+        insertion3(list3, trans);
+    }
+    if(acc > nmb){
+        delete(list3);
+        insertion3(list3,trans);
+    }
+    else{}
+
+}
+}
 
 int main (int argc, char *argv[]){
 
-uint8_t *hashe = malloc(sizeof(char)*32);
-
-
+int nmb = 0;
 int fd, rd;
 fd = open("test.bin",O_RDONLY);
 if(fd < 0){
