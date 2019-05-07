1605fc8b455718efc1bee2f336519126f3b2beb8
arnaudontour <48843478+arnaudontour@users.noreply.github.com>
2019-05-07 15:06:10 +0200

    Add files via upload

--- cracker/src/cracker.c
+++ cracker/src/cracker.c
@@ -1,7 +1,6 @@
 // TOUT LES INCLUDES
 
 #include <semaphore.h>
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,33 +13,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include "sha256.h"
+#include "reverse.h"
 
 // TOUTE LES CONSTANTES
 
 #define N 4 // SLOT DU PREMIER BUFFER
 #define THREADS 4 //nombre de THREADS souhaite
 
-
-//INITIALISATION DES TABLEAU VARIABLE
-
-
-
-int main (int argc, char *argv[]){
-
-
-uint8_t hash[31]; // TABLEAU CONTENANT 32*BYTES
-uint8_t buffer[N-1][31]; // BUFFER CONTENANT N * 32*BYTES
-int err; // variable d'erreur lors de la création de thread
-
-//INITIALISATION DE LA LECTURE DU FICHIER
-
-int fd, rd;
-fd = open(test.bin,O_RDONLY);
-if(fd < 0){
-    return -1;
-}
-
-//INITIALISATION DES THREADS
+//INITIALISATION DES MUTEX ET SEMAPHORE
 
 pthread_mutex_t mutex;
 sem_t empty;
@@ -49,29 +30,159 @@ pthread_mutex_init(&mutex, NULL);
 sem_init (&empty,0,N-1);
 sem_init(&full,0,0);
 
-//PREMIERE FONCTION PRODUCTEUR(REMPLIR DE BUFFER DE HASH)
+//ARRAYLIST BUFFER 1 (32bytes)
 
-void producer(void){
+typedef struct Element Element;
+struct Element
+{
+    Element *suivant;
+    uint8_t *hash[32];
+};
+
+
+typedef struct Liste Liste;
+struct Liste
+{
+    Element *premier;
+};
+
+Liste *initialisation()
+{
+    Liste *liste = malloc(sizeof(*liste));
+    Element *element = malloc(sizeof(*element));
+
+    if (liste == NULL || element == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    element->hash = NULL;
+    element->suivant = NULL;
+    liste->premier = element;
+
+    return liste;
+}
+
+
+void insertion(Liste *liste, uint8_t *value)
+{
+    /* Création du nouvel élément */
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (liste == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    nouveau->hash = *value;
+
+    /* Insertion de l'élément au début de la liste */
+    nouveau->suivant = liste->premier;
+    liste->premier = nouveau;
+}
+
+void suppression(Liste *liste)
+{
+    if (liste == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (liste->premier != NULL)
+    {
+        Element *aSupprimer = liste->premier;
+        liste->premier = liste->premier->suivant;
+        free(aSupprimer);
+    }
+}
+
+//ARRAYLIST BUFFER 2 (liste avec les traductions)
+
+typedef struct Element1 Element1;
+struct Element1
+{
+    Element1 *suivant;
+    char trad[16];
+};
+
+
+typedef struct Liste1 Liste1;
+struct Liste1
+{
+    Element1 *premier;
+};
+
+Liste *initialisation()
+{
+    Liste *liste = malloc(sizeof(*liste));
+    Element *element = malloc(sizeof(*element));
+
+    if (liste == NULL || element == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    element->hash = NULL;
+    element->suivant = NULL;
+    liste->premier = element;
+
+    return liste;
+}
+
+
+void insertion(Liste *liste, uint8_t *value)
+{
+    /* Création du nouvel élément */
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (liste == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    nouveau->hash = *value;
+
+    /* Insertion de l'élément au début de la liste */
+    nouveau->suivant = liste->premier;
+    liste->premier = nouveau;
+}
+
+void suppression(Liste *liste)
+{
+    if (liste == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (liste->premier != NULL)
+    {
+        Element *aSupprimer = liste->premier;
+        liste->premier = liste->premier->suivant;
+        free(aSupprimer);
+    }
+}
+
+//ARRAYMIST BUFFER 3 (List avec X nombre de voyelle ou consonne)
+
+ //Premier producteur
+
+void* producer(void*arg){
 	while(true){
 	sem_wait(&empty);
 	pthread_mutex_lock(&mutex);
 
 
-
+	uint8_t *hashe = malloc(sizeof(char)*32);
 	if(rd!=0){
     
-    rd = read(fd,hash,sizeof(sizeof(char)*32));
+    rd = read(fd,hashe,sizeof(sizeof(char)*32));
     if(rd < 0){
        // return -1;
     }
-    buffer[0][0] = hash;
+    insertion(buffer1,hashe);
+    free(hashe);
 	}
 	if(rd == 0){
 		int cl=close(fd);
 		if (cl < 0){
     	//return -1;
     	}
-	// ECRIRE ICI QUAND LE FICHIER EST VIDE
 	}
 	
 	pthread_mutex_unlock(&mutex);
@@ -79,17 +190,31 @@ void producer(void){
 	}
 }
 
-//PREMIERE FONCTION CONSOMMATEUR(DEHASHER ET METTRE DANS UN AUTRE FICHIER)
+// premier consommateur
+
 void consumer(void){
 	while(true){
 	sem_wait(&full);
 	pthread_mutex_lock(&mutex);
 
 
+
 	pthread_mutex_unlock(&mutex);
 	sem_post(&empty);
 	}
-} 
+}
+ 
+
+int main (int argc, char *argv[]){
+
+uint8_t *hashe = malloc(sizeof(char)*32);
+
+
+int fd, rd;
+fd = open("test.bin",O_RDONLY);
+if(fd < 0){
+    return -1;
+}
 
 
 }
